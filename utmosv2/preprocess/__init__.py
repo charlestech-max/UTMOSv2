d4a8e5d38e5c32cdadb6c98905267b5518e4989c
charlestech-max <charles.tech0x77@outlook.com>
2026-09-18 00:12:16 -0300

    delete format __init__.py

--- utmosv2/preprocess/__init__.py
+++ utmosv2/preprocess/__init__.py
@@ -5,4 +5,4 @@ from utmosv2.preprocess._preprocess import (
     remove_silent_section,
 )
 
-__all__ = ["add_sys_mean", "preprocess", "preprocess_test", "remove_silent_section"]
+__all__ = ["add_sys_mean", "preprocess", "preprocess_test", "remove_silent_section"]
